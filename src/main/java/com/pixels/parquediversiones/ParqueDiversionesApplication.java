761365fc79f1a07a5c957868155c1a612657f514
Kevin Cubillos <kevinlexr@gmail.com>
2023-06-09 02:11:54 -0500

    Create base structure

---
+++ src/main/java/com/pixels/parquediversiones/ParqueDiversionesApplication.java
@@ -0,0 +1,13 @@
+package com.pixels.parquediversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueDiversionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueDiversionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixels/parquediversiones/ParqueDiversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixels.parquediversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueDiversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
